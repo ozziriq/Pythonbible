dd9d9527d9b10c8817cdc53590620291d0a29954
Oskar Karnehall <oskar@karnehall.com>
2021-05-11 16:07:58 +0200

    EoD

---
+++ pythonrandom.py
@@ -0,0 +1,12 @@
+# Just a little mepp.
+
+# Grab the email from a user, and strip spaces
+email = input("What is your email adress? :").strip()
+# Slice out the username and save as a variable.
+username = email[:email.index("@")]
+
+# slice out the domain name
+
+# Format the message
+
+# Display output message 
